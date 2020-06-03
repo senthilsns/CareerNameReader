c6d2b5aba0057c86b9b97862d4752de545f9bff6
Senthil kumar K <nightrider.cbe@gmail.com>
2020-06-03 09:20:00 +0530

    Commit

--- FindCarrierName/FindCarrierName/ViewController.swift
+++ FindCarrierName/FindCarrierName/ViewController.swift
@@ -16,20 +16,47 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         
-        
         // Find Network Carrier Name
-        if #available(iOS 12.0, *) {
-            if let providers = CTTelephonyNetworkInfo().serviceSubscriberCellularProviders {
-                providers.forEach { (key, value) in
-                    print("key: \(key), carrier: \(value.carrierName ?? "nil")")
-                }
-            }
-        } else {
-            let provider = CTTelephonyNetworkInfo().subscriberCellularProvider
-            print("carrier: \(provider?.carrierName ?? "nil")")
-        }
+        FindCarrierDetect()
+        
+        CTTelephonyNetworkInfo().mo
         
         
+    }
+    
+    
+    func FindCarrierDetect()
+    {
+        // Find Network Carrier Name
+              if #available(iOS 12.0, *) {
+                  if let providers = CTTelephonyNetworkInfo().serviceSubscriberCellularProviders {
+                      providers.forEach { (key, value) in
+                          print("key: \(key), carrier: \(value.carrierName ?? "nil")")
+                      }
+                  }
+              } else {
+                  let provider = CTTelephonyNetworkInfo().subscriberCellularProvider
+                  print("carrier: \(provider?.carrierName ?? "nil")")
+              }
+    }
+    
+    
+    func FindIntertConnectivityInSIM() -> Bool
+    {
+        let telephonyManager = CTTelephonyNetworkInfo();
+               if #available(iOS 12.0, *) {
+                   let service = telephonyManager.serviceCurrentRadioAccessTechnology
+                   if (service != nil) && service?.count ?? 0 > 0 {
+                       return true
+                   }
+               } else {
+                   let service = telephonyManager.currentRadioAccessTechnology
+                   if (service != nil) && service?.count ?? 0 > 0 {
+                       return true
+                   }
+               };
+        
+        return false
     }
 
 
